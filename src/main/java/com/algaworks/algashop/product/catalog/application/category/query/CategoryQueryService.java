c195cf6cd91cba6f95cda21ab1f4f1f8779173f6
Bruno Ramos Lemos <bruno.lemos@live.com>
2026-06-23 19:54:05 -0300

    feat: Implementing lastModified MongoDB

--- src/main/java/com/algaworks/algashop/product/catalog/application/category/query/CategoryQueryService.java
+++ src/main/java/com/algaworks/algashop/product/catalog/application/category/query/CategoryQueryService.java
@@ -2,9 +2,11 @@ package com.algaworks.algashop.product.catalog.application.category.query;
 
 import com.algaworks.algashop.product.catalog.application.PageModel;
 
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 public interface CategoryQueryService {
     PageModel<CategoryDetailOutput> filter(CategoryFilter filter);
     CategoryDetailOutput findById(UUID categoryId);
+    OffsetDateTime lastModified();
 }
--- src/main/java/com/algaworks/algashop/product/catalog/infrastructure/persistence/category/CategoryQueryServiceImpl.java
+++ src/main/java/com/algaworks/algashop/product/catalog/infrastructure/persistence/category/CategoryQueryServiceImpl.java
@@ -10,15 +10,18 @@ import com.algaworks.algashop.product.catalog.domain.model.category.Category;
 import com.algaworks.algashop.product.catalog.domain.model.category.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.data.domain.Page;
+import org.bson.Document;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.stereotype.Service;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -80,5 +83,22 @@ public class CategoryQueryServiceImpl implements CategoryQueryService {
         Category category = categoryRepository.findById(categoryId)
                 .orElseThrow(ResourceNotFoundException::new);
         return mapper.convert(category, CategoryDetailOutput.class);
+    }
+
+    @Override
+    public OffsetDateTime lastModified(){
+        Aggregation aggregation = Aggregation.newAggregation(
+                Aggregation.group().max("updatedAt").as("lastModified")
+        );
+
+        AggregationResults<Document> result = mongoOperations.aggregate(aggregation, "categories", Document.class);
+
+        Document document = result.getUniqueMappedResult();
+
+        if (document == null) {
+            return OffsetDateTime.now();
+        }
+
+        return document.getDate("lastModified").toInstant().atOffset(ZoneOffset.UTC);
     }
 }
