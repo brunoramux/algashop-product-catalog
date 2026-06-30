2026a625dee9af1f3adeca707bc8c0b17abb0422
Bruno Ramos Lemos <bruno.lemos@live.com>
2026-06-29 21:41:16 -0300

    feat: resource server configuration for product catalog microservice

--- src/main/java/com/algaworks/algashop/product/catalog/infrastructure/security/ProductCatalogSecurityConfig.java
+++ src/main/java/com/algaworks/algashop/product/catalog/infrastructure/security/ProductCatalogSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -15,17 +16,17 @@ public class ProductCatalogSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.disable())
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
                 .sessionManagement(
         sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(
-                        auth -> auth.anyRequest().permitAll()
+                        auth -> auth.anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(
+                        oauth2 -> oauth2.jwt(Customizer.withDefaults())
                 );
-//                .oauth2ResourceServer(
-//                        oauth2 -> oauth2.jwt(Customizer.withDefaults())
-//                );
 
         return http.build();
     }
